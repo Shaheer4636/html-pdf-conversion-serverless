a94adc4edee9e29331a542e069f9523668ab0c4d
Shaheer <muhammadshaheer4636@gmail.com>
2025-10-03 10:03:57 -0700

    Update main.tf

--- datapipeline-lambda/main.tf
+++ datapipeline-lambda/main.tf
@@ -1,27 +1,6 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws    = { source = "hashicorp/aws",    version = ">= 5.40.0" }
-    random = { source = "hashicorp/random", version = ">= 3.5.1" }
-    archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-resource "random_id" "suffix" {
-  count       = var.add_random_suffix ? 1 : 0
-  byte_length = 2
-}
-
-#locals
-locals {
-  fn_name = lower(
-    var.add_random_suffixterraform {
-  required_version = ">= 1.6.0"
-  required_providers {
     aws     = { source = "hashicorp/aws",     version = ">= 5.40.0" }
     random  = { source = "hashicorp/random",  version = ">= 3.5.1" }
     archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
@@ -83,62 +62,7 @@ resource "aws_lambda_function" "uptime" {
 
       ONLY_BROWSER   = var.only_browser
       FAIL_STREAK    = tostring(var.fail_streak)
-      TREAT_MISSING  = tostring(var.treat_missing) # "true"/"false"
-      SLO_TARGET     = var.slo_target
-    }
-  }
-
-  tags = {
-    Project = var.name_prefix
-    Region  = var.region
-  }
-}
-
-output "lambda_function_name" { value = aws_lambda_function.uptime.function_name }
-output "lambda_function_arn"  { value = aws_lambda_function.uptime.arn }
-      ? "${var.name_prefix}-uplambda-${random_id.suffix[0].hex}"
-      : "${var.name_prefix}-uplambda"
-  )
-
-  art_prefix = var.artifact_prefix != "" ? var.artifact_prefix : "canary/${var.region}/${var.name_prefix}"
-}
-
-# Zip the python file as lambda_function.py
-data "archive_file" "zip" {
-  type        = "zip"
-  output_path = "${path.module}/lambda.zip"
-
-  source {
-    filename = "lambda_function.py"
-    content  = file(var.source_py)
-  }
-}
-
-resource "aws_lambda_function" "uptime" {
-  function_name    = local.fn_name
-  role             = var.lambda_role_arn
-  handler          = "lambda_function.handler"
-  runtime          = "python3.11"
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = data.archive_file.zip.output_base64sha256
-  memory_size      = 1024
-  timeout          = 900
-  architectures    = ["x86_64"]
-
-  environment {
-    variables = {
-      ART_BUCKET     = var.artifact_bucket
-      ART_PREFIX     = local.art_prefix
-      REPORTS_BUCKET = var.reports_bucket
-      REPORTS_PREFIX = var.reports_prefix
-
-      COMPANY        = var.company
-      SERVICE        = var.service
-      CLIENT         = var.client
-
-      ONLY_BROWSER   = var.only_browser
-      FAIL_STREAK    = tostring(var.fail_streak)
-      TREAT_MISSING  = tostring(var.treat_missing)  # "true"/"false"
+      TREAT_MISSING  = tostring(var.treat_missing)
       SLO_TARGET     = var.slo_target
     }
   }
